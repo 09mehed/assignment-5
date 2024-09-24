11f7f859c6b0bd31cb8a2b5bfc04ea31d9a4efbf
09mehed <foysalalmamun558@gmail.com>
2024-09-24 09:28:38 +0600

    blog area complete

--- index.js
+++ index.js
@@ -10,19 +10,24 @@ donateFirstCard.addEventListener('click', function(){
 
     const mainAmountElement = document.getElementById('main-amount');
     const mainAmount = parseFloat(mainAmountElement.innerText);
+
     const mainAmountReceived = mainAmount + inputAmount;
     mainAmountElement.innerText = mainAmountReceived.toFixed(2);
 
     const historyItem = document.createElement('div');
     historyItem.className = "bg-white p-3 rounded-md border border-gray-200"
     historyItem.innerHTML = `
-        <p class="text-xl text-black">${main-amount} Donate for Flood at Noakhali, Bangladesh</p>
+        <p class="text-xl text-black">${mainAmountReceived} Donate for Flood at Noakhali, Bangladesh</p>
         <p class="text-xl text-black">${new Date().toLocaleDateString()}</p>
     `
     const historyList = document.getElementById('history-list');
     historyList.insertBefore(historyItem, historyList.firstChild)
 
     if(inputAmount <= 0 || isNaN(inputAmount)){
+        alert ("Please Input a Valid Number"); 
+    }
+
+    if(inputAmount > mainAmount){
         alert ("Please Input a Valid Number"); 
     }
 })
@@ -42,9 +47,18 @@ donateFirstCard2.addEventListener('click', function(){
 
     const mainAmountElement = document.getElementById('main-amount');
     const mainAmount = parseFloat(mainAmountElement.innerText);
+
     const mainAmountReceived = mainAmount + inputAmount;
     mainAmountElement.innerText = mainAmountReceived.toFixed(2);
 
+    const historyItem = document.createElement('div');
+    historyItem.className = "bg-white p-3 rounded-md border border-gray-200"
+    historyItem.innerHTML = `
+        <p class="text-xl text-black">${mainAmountReceived} Donate for Flood Relief in Feni, Bangladesh</p>
+        <p class="text-xl text-black">${new Date().toLocaleDateString()}</p>
+    `
+    const historyList = document.getElementById('history-list');
+    historyList.insertBefore(historyItem, historyList.firstChild)
 
     if(inputAmount <= 0 || isNaN(inputAmount)){
         alert ("Please Input a Valid Number"); 
@@ -68,10 +82,18 @@ donateFirstCard3.addEventListener('click', function(){
     const mainAmountReceived = mainAmount + inputAmount;
     mainAmountElement.innerText = mainAmountReceived.toFixed(2);
 
-    document.getElementById('btn-continue').addEventListener('click', function(){
-        const popUp = document.getElementById('my_modal_1');
-        popUp.style.display = 'none';
-    })
+    const historyItem = document.createElement('div');
+    historyItem.className = "bg-white p-3 rounded-md border border-gray-200"
+    historyItem.innerHTML = `
+        <p class="text-xl text-black">${mainAmountReceived} Aid for Injured in the Quota Movement</p>
+        <p class="text-xl text-black">${new Date().toLocaleDateString()}</p>
+    `
+    const historyList = document.getElementById('history-list');
+    historyList.insertBefore(historyItem, historyList.firstChild)
+    // document.getElementById('btn-continue').addEventListener('click', function(){
+    //     const popUp = document.getElementById('my_modal_1');
+    //     popUp.style.display = 'none';
+    // })
 
     if(inputAmount <= 0 || isNaN(inputAmount)){
         alert ("Please Input a Valid Number");
@@ -111,7 +133,7 @@ historyTab.addEventListener('click', function(){
     sectionForm.classList.add('hidden');
 
     const historySection = document.getElementById('history-section');
-    historySection.classList.add('hidden');
+    historySection.classList.remove('hidden');
 })
 
 donationTab.addEventListener('click', function(){
